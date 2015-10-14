7d28808d468b8dc1d16e380fbb81f19b47de5e70
marcomag <marcomagnetti@gmail.com>
2015-10-14 04:18:21 +0200

    Update collections for: - Native search support - Native sort support - Url encoded cursors

--- Graphene/imports/Collections/Collections.php
+++ Graphene/imports/Collections/Collections.php
@@ -13,24 +13,32 @@ class ReadCollection extends Action
         $query      = $this->request->getPar('search');
         $sortBy     = $this->request->getPar('sortBy');
         $sortMode   = $this->request->getPar('sortMode');
-
         $page     = $this->request->getPar('page');
         $pageSize = $this->request->getPar('page_size');
+
+        $httpQ =http_build_query([
+            'search'  => $query,
+            'sortBy'  => $sortBy,
+            'sortMode'=> $sortMode
+        ]);
+
+        if($httpQ !== '')$httpQ='?'.$httpQ;
+
         $gQuery=[
             'search'=>$query,
             'sort'  =>['by'=>$sortBy,'mode'=>$sortMode]
         ];
         $model->setLazy(true);
-
         $readed = $model->read(true,$gQuery,$page,$pageSize);
+
         if($readed instanceof ModelCollection){
             $expl = explode('/collection', $this->request->getUrl());
             $url  ='http://'.$_SERVER['HTTP_HOST'].Graphene::getInstance()->getSettings()['baseUrl'].$expl[0].'/collection/';
             $page     = $readed->getPage();
             $pageSize = $readed->getPageSize();
-            $readed->setNextPageUrl     ($url.($page+1).'/'.$pageSize);
-            $readed->setCurrentPageUrl  ($url.($page).'/'.$pageSize);
-            if($page > 1) $readed->setPreviousPageUrl ($url.($page-1).'/'.$pageSize);
+            $readed->setNextPageUrl     ($url.($page+1).'/'.$pageSize.$httpQ);
+            $readed->setCurrentPageUrl  ($url.($page).'/'.$pageSize.$httpQ);
+            if($page > 1) $readed->setPreviousPageUrl ($url.($page-1).'/'.$pageSize.$httpQ);
         }
         $this->sendModel($readed);
 
