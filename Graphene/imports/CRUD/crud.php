cd6395d42582e4e332277cf73a632239f76283f4
marcomag <marcomagnetti@gmail.com>
2015-11-10 00:53:56 +0100

    Activity crud mods

--- Graphene/imports/CRUD/crud.php
+++ Graphene/imports/CRUD/crud.php
@@ -2,6 +2,7 @@
 namespace imports;
 
 use Graphene\controllers\Action;
+use Graphene\models\Model;
 
 class Create extends Action
 {
@@ -23,6 +24,43 @@ class Create extends Action
         $model = new $this->pars[0]();
         return [$model->getModelName()=> $model->getCreateActionStruct()];
     }
+
+    public function getActionInterface()
+    {
+        $model = new $this->pars[0]();
+        $struct = [$model->getModelName() => $model->getCreateActionStruct()];
+        $flatStructArr = $this->contentToFlatArray($struct);
+        $flatStruct = [];
+        foreach ($flatStructArr as $k => $fieldStruct) {
+            $flatStructArr[$k] = explode(Model::CHECK_SEP, $fieldStruct);
+            $flatStruct[$k] = [];
+            foreach ($flatStructArr[$k] as $check) {
+                if ($check !== '') {
+                    $flatStruct[$k][] = $check;
+                }
+            }
+        }
+        return [
+            "name" => "STD_CREATE",
+            "struct" => $struct,
+            "flat-struct" => $flatStruct
+        ];
+    }
+
+    private function contentToFlatArray($content, &$path = '', &$schema = null)
+    {
+        if ($schema == null) $schema = array();
+        foreach ($content as $key => $value) {
+            if (strcmp($path, '') == 0) $tmpPath = $key;
+            else $tmpPath = $path . '_' . $key;
+
+            if (is_array($value) && $content != NULL) $this->contentToFlatArray($value, $tmpPath, $schema);
+            else {
+                $schema[$tmpPath] = $value;
+            }
+        }
+        return $schema;
+    }
 }
 
 class Read extends Action{
@@ -39,6 +77,43 @@ class Read extends Action{
     public function getResponseStruct(){
         $model = new $this->pars[0]();
         return [$model->getModelName() => $model->getReadActionStruct()];
+    }
+
+    public function getActionInterface()
+    {
+        $model = new $this->pars[0]();
+        $struct = [$model->getModelName() => $model->getReadActionStruct()];
+        $flatStructArr = $this->contentToFlatArray($struct);
+        $flatStruct = [];
+        foreach ($flatStructArr as $k => $fieldStruct) {
+            $flatStructArr[$k] = explode(Model::CHECK_SEP, $fieldStruct);
+            $flatStruct[$k] = [];
+            foreach ($flatStructArr[$k] as $check) {
+                if ($check !== '') {
+                    $flatStruct[$k][] = $check;
+                }
+            }
+        }
+        return [
+            "name" => "STD_READ",
+            "struct" => $struct,
+            "flat-struct" => $flatStruct
+        ];
+    }
+
+    private function contentToFlatArray($content, &$path = '', &$schema = null)
+    {
+        if ($schema == null) $schema = array();
+        foreach ($content as $key => $value) {
+            if (strcmp($path, '') == 0) $tmpPath = $key;
+            else $tmpPath = $path . '_' . $key;
+
+            if (is_array($value) && $content != NULL) $this->contentToFlatArray($value, $tmpPath, $schema);
+            else {
+                $schema[$tmpPath] = $value;
+            }
+        }
+        return $schema;
     }
 }
 
@@ -62,6 +137,43 @@ class Update extends Action
         $model = new $this->pars[0]();
         return [$model->getModelName() => $model->getUpdateActionStruct()];
     }
+
+    public function getActionInterface()
+    {
+        $model = new $this->pars[0]();
+        $struct = [$model->getModelName() => $model->getUpdateActionStruct()];
+        $flatStructArr = $this->contentToFlatArray($struct);
+        $flatStruct = [];
+        foreach ($flatStructArr as $k => $fieldStruct) {
+            $flatStructArr[$k] = explode(Model::CHECK_SEP, $fieldStruct);
+            $flatStruct[$k] = [];
+            foreach ($flatStructArr[$k] as $check) {
+                if ($check !== '') {
+                    $flatStruct[$k][] = $check;
+                }
+            }
+        }
+        return [
+            "name" => "STD_UPDATE",
+            "struct" => $struct,
+            "flat-struct" => $flatStruct
+        ];
+    }
+
+    private function contentToFlatArray($content, &$path = '', &$schema = null)
+    {
+        if ($schema == null) $schema = array();
+        foreach ($content as $key => $value) {
+            if (strcmp($path, '') == 0) $tmpPath = $key;
+            else $tmpPath = $path . '_' . $key;
+
+            if (is_array($value) && $content != NULL) $this->contentToFlatArray($value, $tmpPath, $schema);
+            else {
+                $schema[$tmpPath] = $value;
+            }
+        }
+        return $schema;
+    }
 }
 
 class Delete extends Action
@@ -75,9 +187,47 @@ class Delete extends Action
         $this->send($model->getModelName() . ' ' . $model->getId() . ', successfully deleted');
     }
 
+
+    public function getActionInterface()
+    {
+        $model = new $this->pars[0]();
+        $struct = [$model->getModelName() => $model->getDeleteActionStruct()];
+        $flatStructArr = $this->contentToFlatArray($struct);
+        $flatStruct = [];
+        foreach ($flatStructArr as $k => $fieldStruct) {
+            $flatStructArr[$k] = explode(Model::CHECK_SEP, $fieldStruct);
+            $flatStruct[$k] = [];
+            foreach ($flatStructArr[$k] as $check) {
+                if ($check !== '') {
+                    $flatStruct[$k][] = $check;
+                }
+            }
+        }
+        return [
+            "name" => "STD_UPDATE",
+            "struct" => $struct,
+            "flat-struct" => $flatStruct
+        ];
+    }
+
+    private function contentToFlatArray($content, &$path = '', &$schema = null)
+    {
+        if ($schema == null) $schema = array();
+        foreach ($content as $key => $value) {
+            if (strcmp($path, '') == 0) $tmpPath = $key;
+            else $tmpPath = $path . '_' . $key;
+
+            if (is_array($value) && $content != NULL) $this->contentToFlatArray($value, $tmpPath, $schema);
+            else {
+                $schema[$tmpPath] = $value;
+            }
+        }
+        return $schema;
+    }
+
     public function getResponseStruct(){
         $model = new $this->pars[0]();
-        return [$model->getModelName() => $model->getReadActionStruct()];
+        return 'message';
     }
 
     public function getRequestStruct(){
