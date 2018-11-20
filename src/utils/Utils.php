fd5e5d59e3923043f14454a0b95ed6fc844a2f85
Antonio Arcucci <arcucci@gmail.com>
2018-11-20 19:11:30 +0100

    Add getToken function to Utils

--- src/utils/Utils.php
+++ src/utils/Utils.php
@@ -2,4 +2,19 @@
 
 namespace Graphene\utils;
 
+class Utils {
+    static public function getToken($length)
+    {
+        $token = "";
+        $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //$codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
+        $codeAlphabet .= "0123456789";
+        $max = strlen($codeAlphabet); // edited
 
+        for ($i = 0; $i < $length; $i++) {
+            $token .= $codeAlphabet[crypto_rand_secure(0, $max - 1)];
+        }
+
+        return $token;
+    }
+}
