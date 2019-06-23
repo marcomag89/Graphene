1c69e1057b6ad78dff594b5a3c1812738c0249c1
Marco Magnetti <marco.magnetti@nsone.it>
2019-06-23 13:23:03 +0200

    Updated mailer for multiple steps management

--- src/utils/Mailer.php
+++ src/utils/Mailer.php
@@ -13,38 +13,72 @@ use PHPMailer\PHPMailer\PHPMailer;
  * Time: 01:00
  */
 class Mailer {
-    static function send($to, $subject, $template, $data) {
+    protected $config;
+    protected $logger;
+    protected $phpMailer;
+    protected $appName;
+
+    public function __construct() {
+        $this->config = Graphene::getInstance()->getSettings()->getSettingsArray()['notifications']['mail'];
+        $this->logger = Graphene::getLogger('graphene_mailer');
+        $this->phpMailer = new PHPMailer();
+        $this->phpMailer->Debugoutput = function ($str, $level) { Graphene::getLogger('mailer')->debug($str); };
+        $this->appName =  Graphene::getInstance()->getSettings()->getSettingsArray()['appName'];
+    }
+
+    public function prepare($subject) {
+        // Preparing mailer
+        $this->phpMailer->SMTPDebug = 2;                           // Enable verbose debug output
+
+        $this->phpMailer->CharSet = 'UTF-8';
+        //Server settings
+        $this->phpMailer->IsSMTP();
+        $this->phpMailer->Host = $this->config['host'];                    // Specify main and backup SMTP servers
+        $this->phpMailer->SMTPAuth = $this->config['auth_enabled'];        // Enable SMTP authentication
+        $this->phpMailer->Username = $this->config['user'];                // SMTP username
+        $this->phpMailer->Password = $this->config['password'];            // SMTP password
+        $this->phpMailer->SMTPSecure = $this->config['secure'];            // Enable TLS encryption, `ssl` also accepted
+        $this->phpMailer->Port = $this->config['port'];                    // TCP port to connect to
+        $this->phpMailer->setFrom($this->config['from'], $this->appName);
+
+        $this->phpMailer->Subject = $subject;
+        return $this->phpMailer;
+    }
+
+    public function addB64Image($name, $b64) {
+        $this->phpMailer->addStringAttachment(base64_decode($b64), $name);
+    }
+
+    /**
+     * @param $mail {PHPMailer}
+     * @param $template
+     * @param $data
+     * @return mixed
+     */
+    public function setHtmlBody($body, $data = []) {
+        // processing template
+        $m = new Mustache_Engine();
+        $rBody = $m->render($body, $data);
+
+        $this->phpMailer->IsHTML(true);
+        $this->phpMailer->Body = $rBody;
+
+        return $this->phpMailer;
+    }
+
+    public function sendTo($address) {
         try {
-            $config = Graphene::getInstance()->getSettings()->getSettingsArray()['notifications']['mail'];
-
-            // Preparing mailer
-            $mail = new PHPMailer(true);
-            $mail->Debugoutput = function ($str, $level) { Graphene::getLogger('mailer')->debug($str); };
-            $mail->SMTPDebug = 2;                           // Enable verbose debug output
-
-            $mail->CharSet = 'UTF-8';
-            //Server settings
-            $mail->IsSMTP();
-            $mail->IsHTML(true);
-            $mail->Host = $config['host'];                    // Specify main and backup SMTP servers
-            $mail->SMTPAuth = $config['auth_enabled'];        // Enable SMTP authentication
-            $mail->Username = $config['user'];                // SMTP username
-            $mail->Password = $config['password'];            // SMTP password
-            $mail->SMTPSecure = $config['secure'];            // Enable TLS encryption, `ssl` also accepted
-            $mail->Port = $config['port'];                    // TCP port to connect to
-            $mail->setFrom($config['from'], $config['appName']);
-
-            //Setting contents
-            $mail->addAddress($to);
-            $mail->Subject = $subject;
-
-            // processing template
-            $m = new Mustache_Engine;
-            $mail->Body = $m->render($template, $data);
-
-            $mail->send();
+            $this->phpMailer->addAddress($address);
+            $this->phpMailer->send();
         } catch (\Exception $e) {
-            Graphene::getLogger('graphene_mailer')->error($e);
+            $this->logger->error($e);
         }
+    }
+
+    static function send($to, $subject, $template, $data) {
+        $mailer = new Mailer();
+        $mailer->prepare($subject);
+        $mailer->setHtmlBody($template, $data);
+        $mailer->sendTo($to);
     }
 }
